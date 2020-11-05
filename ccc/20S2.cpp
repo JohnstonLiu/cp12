25ea3ef79c71d964c7d25a826a48f133dde880ba
Johnston Liu <johnstonliu2004@gmail.com>
2020-11-05 10:14:26 -0800

    improved 20s2 13/15

--- ccc/20S2.cpp
+++ ccc/20S2.cpp
@@ -1,62 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 int M, N;
-int a[1010][1010];
-vector<int> dp[1000010];
-
-
-void factor(int n) {
-    if(dp[n].empty()) {
-        for(int i=1; i<=(int)sqrt(n) && i<=M; ++i) {
-            if(n%i==0) {
-                if(n/i<=N) {
-                    dp[n].push_back(i); dp[n].push_back(n/i);
-                }
-                if(i<=N && n/i<=M) {
-                    dp[n].push_back(n/i); dp[n].push_back(i);
-                }
-            }
-        }
-    }
-}
+int room[1010][1010];
+vector<int> adj[1000001];
 
 void BFS() {
-    int temp=1, temp1=1;
-    queue<int> q; bool visited[1000001];
+    queue<int> q;
+    bool visited[1000001];
 
-    visited[a[temp][temp1]]=true;
-    q.push(temp); q.push(temp1);
+    q.push(M); q.push(N);
     while(!q.empty()) {
         int r=q.front(); q.pop();
         int c=q.front(); q.pop();
-
-        if(r==M && c==N) {
+        if(r==1 && c==1) {
             cout << "yes";
             return;
         }
-
-        int n=a[r][c];
-        factor(n);
-
-        for(int i=0, j=1; j<dp[n].size(); i+=2, j+=2) {
-            if(!visited[a[dp[n].at(i)][dp[n].at(j)]]) {
-                q.push(dp[n].at(i)); q.push(dp[n].at(j));
+        if(!visited[r*c]){
+            for(auto u : adj[r*c]) {
+                q.push(u);
             }
+            visited[r*c]=true;
         }
-        visited[n]=true;
     }
     cout << "no";
 }
-
 
 int main() {
     cin >> M >> N;
     for(int i=1; i<=M; ++i) {
         for(int j=1; j<=N; ++j) {
-            int n; cin >> n;
-            a[i][j]=n;
+            cin >> room[i][j];
+            adj[room[i][j]].push_back(i);
+            adj[room[i][j]].push_back(j);
         }
     }
-
     BFS();
 }
