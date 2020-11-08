03e695bd9d5bc256f6c14bf9f80bd47a769334c1
Johnston Liu <johnstonliu2004@gmail.com>
2020-11-08 15:58:08 -0800

    finished 16S4

--- ccc/16S4.cpp
+++ ccc/16S4.cpp
@@ -3,21 +3,10 @@ using namespace std;
 
 
 int af[401][401]={};
-int as[401][401]={};
-int ar[401]={};
+int as[401]={};
 
 int sum(int i, int j) {
-    int a=i, b=j;
-    if(as[i][j]!=0) {
-        return as[i][j];
-    }
-    int s=0;
-    while(i<=j) {
-        s+=ar[i];
-        i++;
-    }
-    as[a][b]=s;
-    return s;
+    return as[j]-as[i-1];
 }
 
 int f(int i, int j) {
@@ -25,15 +14,15 @@ int f(int i, int j) {
         return 1;
     }
 
-    if(af[i][j]!=0) {
+    if(af[i][j]!=-1) {
         return af[i][j];
     }
     int m=0;
     for(int a=i; a<j; ++a) {
-        for(int b=a+1; b<=j; ++b) {
-            int x=f(i,a); int y=f(a+1,b-1); int z=f(b,j);
+        for(int b=j; b>=a+1; --b) {
             if(sum(i,a)==sum(b,j)) {
-                m=max(m,x*y*z);
+                int x=f(i,a); int y=f(a+1,b-1); int z=f(b,j);
+                m=max(m, x*y*z);
             }
         }
     }
@@ -47,7 +36,14 @@ int main() {
     cin >> n;
 
     for(int i=1; i<=n; ++i) {
-        cin >> ar[i];
+        for(int j=1; j<=n; ++j) {
+            af[i][j]=-1;
+        }
+    }
+
+    for(int i=1; i<=n; ++i) {
+        int a; cin >> a;
+        as[i]=as[i-1]+a;
     }
 
     int m=0;
