9f3cb0966eb05a7c6f26ec51db00f355cd994ce0
Johnston Liu <johnstonliu2004@gmail.com>
2020-11-18 11:43:09 -0800

    update read me and 02S4

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Problems | Status | Type
 # Minimester 2
 Problems | Status | Type
 ---------|--------|-----
+OOS4 | TLE | Dynamic Programming
+02S4 | WA | Dynamic Programming
 07S4 | Done | Dynamic Programming
 15S1 | Done | Big Brain Implementation
 16S4 | Done | Dynamic Programming
--- ccc/02S4.cpp
+++ ccc/02S4.cpp
@@ -9,8 +9,6 @@ int main() {
     string names[Q];
 
     for(int i=0; i<Q; ++i) {
-        //m-1 loop times and the remaining gets brain dead solved
-        //remember the drawing you did
         //if this doesn't work try out the brute force method
         //if its divisible you already know M
         cin >> names[i];
