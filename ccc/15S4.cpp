c0104cd1396a2592925ef9e6459fa51938bcaf4a
Johnston Liu <johnstonliu2004@gmail.com>
2020-12-06 18:11:54 -0800

    vector arrays

--- ccc/15S4.cpp
+++ ccc/15S4.cpp
@@ -1,17 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int K, N, M;
-int a[2001][2001]={};
-int b[2001][2001]={};
+vector<int> weight[2001]={};
+vector<int> damage[2001]={};
 vector<int> adj[2001]={};
-int w[2001];
+//use the big brain vector<int>, int instead of int, int`
+int h[2001];
 int d[2001];
-typedef pair<int, int> ii;
+typedef pair<vector<int>, int> ii;
 priority_queue<ii, vector<ii>, greater<ii>> p;
 
 void initialize_ss(int s) {
     for(int i=1; i<2001; ++i) {
-        w[i]=N;
+        h[i]=K;
     }
     for(int i=1; i<2001; ++i) {
         d[i]=INT_MAX;
@@ -22,15 +23,19 @@ void initialize_ss(int s) {
 void dijkstra(int s) {
     bool processed[2001]={};
     initialize_ss(s);
-    for(int i=0; i<N; ++i) {
-        p.push(make_pair(d[i+1], i+1));
+    for(int i=1; i<N; ++i) {
+        vector<int> v; v.push_back(d[i]);
+        p.push(make_pair(v, i));
     }
 
     while(!p.empty()) {
         int u=p.top().second; p.pop();
         if(processed[u])continue;
-        for(auto v : adj[u]) {
-            if(d[v]>d[u]+a[u][v]) {
+        for(int i=0; i<adj[u].size(); ++i) {
+            //for
+            int v=adj[u][i];
+            if(d[v]>d[u]+weight[u][i] && (h[u]-b[u][v])>0) {
+                h[v]=[u]-b[u][v];
                 d[v]=d[u]+a[u][v];
                 p.push(make_pair(d[v], v));
             }
@@ -42,18 +47,21 @@ int main() {
     cin >> K >> N >> M;
 
     for(int i=0; i<M; ++i) {
-        int x, y;
-        cin >> x >> y;
+        int x, y, a, b;
+        cin >> x >> y >> a >> b;
         adj[x].push_back(y);
-        cin >> a[x][y];
-        cin >> b[x][y];
+        adj[y].push_back(x);
+        weight[x].push_back(a);
+        weight[y].push_back(a);
+        damage[x].push_back(b);
+        damage[y].push_back(b);
     }
     int s, e;
     cin >> s >> e;
     dijkstra(s);
-    for(int i=0; i<2001; ++i) {
-        if(d[i]!=INT_MAX) {
-            cout << i << ": " << d[i] << endl; 
-        }
+    if(d[e]==INT_MAX) {
+        cout << -1;
+    }else {
+        cout << d[e];
     }
 }
