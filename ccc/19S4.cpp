7bd76cc5fd8a7dbac0f263ff49721e40d8253c3e
Johnston Liu <johnstonliu2004@gmail.com>
2021-01-10 11:13:29 -0800

    started 19S4

--- ccc/19S4.cpp
+++ ccc/19S4.cpp
@@ -1,2 +1,13 @@
 //test each posible irregular group and then sum the max.
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int N, K;
+    cin >> N >> K;
+    int a[N];
+    for(int i=0; i<N; ++i) {
+        cin >> a[i];
+    }    
+    int r=N%K
+}
