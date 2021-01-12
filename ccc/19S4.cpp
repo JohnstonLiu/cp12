79ef471b8bcb388d224adc366c63d006cf26dd1c
Johnston Liu <johnstonliu2004@gmail.com>
2021-01-12 15:47:24 -0800

    yes

--- ccc/19S4.cpp
+++ ccc/19S4.cpp
@@ -1,24 +1,87 @@
-//test each posible irregular group and then sum the max.
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int N, K;
     cin >> N >> K;
-    int a[N];
+    int a[100001]={};
     for(int i=0; i<N; ++i) {
         cin >> a[i];
     }    
-    int rem=N%K;
-    int k[N];
-    int r[N];
-    //O(n) implementation of creating the k and r arrays
+    int R=N%K;
+
+    int k[100000]={};
+    int r[100000]={};
+
     int m=0;
-    for(int i=0; i<N; ++i) {
+    for(int i=0; i<=N; ++i) {
         if(i!=0 && i%K==0) {
+            k[i-K]=m;
             m=0;
         }
         m=max(m, a[i]);
     }
-    
+    m=0;
+    for(int i=R; i<=N; ++i) {
+        if(i-R!=0 && (i-R)%K==0) {
+            k[i-K]=m;
+            m=0;
+        }
+        m=max(m, a[i]);
+    }
+    m=0;
+    int counter=1;
+    for(int i=0; i<=N; ++i, ++counter) {
+        if(i!=0 && counter==R+1) {
+            r[i-R]=m;
+            counter=1;
+            m=0;
+            i+=(K-R);
+        }
+        m=max(m, a[i]);
+    }
+
+    for(int i=0; i<N; ++i) {
+        cout << a[i];
+    }
+    cout << endl;
+
+    for(int i=0; i<N; ++i) {
+        if(k[i]==0) {
+            cout << " ";
+        }else {
+            cout << k[i];
+        }
+    }
+    cout << endl;
+
+    for(int i=0; i<N; ++i) {
+        if(r[i]==0) {
+            cout << " ";
+        }else {
+            cout << r[i];
+        }
+    }
+
+
+    int sum=0;
+    sum+=r[0];
+    for(int i=R; i<N; i+=K) {
+        sum+=k[i];
+    }
+    for(int i=0; i<N-R; i+=K) {
+        int prev=sum;
+        sum-=r[i];
+        sum+=k[i];
+        sum-=k[i+R];
+        sum+=r[i+K];
+        sum=max(sum, prev);
+    }
+    int final=0;
+    for(int i=0; i<N-R; i+=K) {
+        final+=k[i];
+    }
+    final+=r[N-R];
+    sum=max(sum, final);
+    cout << sum;
 }
