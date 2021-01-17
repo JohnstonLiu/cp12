f23705c45ea29b434a1a8bdf2bf5cd3bda032c35
Johnston Liu <johnstonliu2004@gmail.com>
2021-01-16 22:36:55 -0800

    finished golf with textbook hacks

--- ccc/00S4.cpp
+++ ccc/00S4.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
+#define INF 1000000007
 using namespace std;
 
 int len;
-int coins[32];
-int solve(int x) {
-    if(x<0)return INFINITY;
-    if(x==0)return 0;
-    int best=INFINITY;
-    for(int i=0; i<len; ++i) {
-        best=min(best, solve(x-coins[i])+1);
+int coins[33];
+int value[5281];
+int solve(int n) {
+    value[0]=0;
+    for(int x=1; x<=n; ++x) {
+        value[x]=INF;
+        for(int i=0; i<len; ++i) {
+            if(x-coins[i]>=0) {
+                value[x]=min(value[x], value[x-coins[i]]+1);
+            }
+        }
     }
-    return best;
+    return value[n];
 }
 
 int main() {
@@ -20,6 +25,10 @@ int main() {
     for(int i=0; i<len; ++i) {
         cin >> coins[i];
     }
-    sort(coins, coins+len);
-    cout << solve(x);
+    int ans=solve(x);
+    if(ans!=INF) {
+        cout << "Roberta wins in " << solve(x) << " strokes.";
+    }else {
+        cout << "Roberta acknowledges defeat.";
+    }
 }
