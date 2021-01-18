f3d2032f1da6f88550e6120e85eefff2813f2dc2
Johnston Liu <johnstonliu2004@gmail.com>
2021-01-17 16:20:02 -0800

    started hashing

--- ccc/20S3.cpp
+++ ccc/20S3.cpp
@@ -4,10 +4,24 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pi;
 #define INF 1000000007
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+
+int A=31; int B=INF;
+int* h;
+int* p;
+ll hsh(string str) {
+    int n=str.length();
+    ll hash=0;
+    ll pp=0;
+    for(int i=0; i<n; ++i) {
+        int x=pow(A, n-1-i);
+        hash+=str[i]*x;
+        pp+=x;
+        h[i]=hash;
+        p[i]=pp;
+    }
+    return hash;
+}
+
 bool check(int a[], int b[]) {
     for(int i=0; i<26; ++i) {
         if(a[i]!=b[i]) {
@@ -26,7 +40,8 @@ int main() {
     int ans=0;
     int dp[26]={};
     int foo[26]={};
-    unordered_map<string, bool> hash={};
+    unordered_map<int, bool>map;
+
     for(char c : needle) {
         ++dp[c-97];
     }
@@ -41,19 +56,20 @@ int main() {
     for(int i=0; i<n; ++i) {
         foo[haystack[i]-97]++;
     }
+    hsh(haystack);
     if(check(dp, foo)){
         ans++;
         string temp=haystack.substr(0, n);
-        hash[temp]=true;
+        map[hash]=true;
     }
     for(int i=0; i<h-n; ++i) {
         foo[haystack[i]-97]--;
         foo[haystack[i+n]-97]++;
         if(check(dp, foo)){
             string str=haystack.substr(i+1, n);
-            if(!hash[str]) {
-                hash[str]=true;
+            if(!map[hash]) {
                 ans++;
+                map[hash]=true;
             }
         }
     }
