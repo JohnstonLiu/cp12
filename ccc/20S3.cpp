b346a2e3a4f55c75cf31ce5b577f56434d089623
Johnston Liu <johnstonliu2004@gmail.com>
2021-01-19 17:23:32 -0800

    hashing

--- ccc/20S3.cpp
+++ ccc/20S3.cpp
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-#define INF 1000000007
 
-int A=31; int B=INF;
-int* h;
-int* p;
-void hsh(string str) {
-    ll hash=0;
-    int n=str.length();
-    for(int i=0; i<n; ++i) {
-        ll pw=pow(A, n-i-1);
-        h[i+1]=(hash+=str[i]*pw)%B;
-        p[i+1]=pw%B;
+ll* h; ll* p;
+const ll A=31; const ll B=10000000009;
+void init(string str) {
+    h[0]=str[0];
+    p[0]=1;
+    for(int i=1; i<str.length(); ++i) {
+        h[i]=(((h[i-1]*A+str[i])%B)+B)%B;
+        p[i]=(((p[i-1]*A)%B)+B)%B;
     }
 }
 
-int id(int a, int b) {
-   if(a==0) {
-       return h[b];
-   } 
-   return(h[b]-h[a-1]*p[b-a+1])%B;
+ll hsh(int a, int b) {
+    if(a==0) {
+        return h[b];
+    }
+    return (((h[b]-h[a-1]*p[b-a+1])%B)+B)%B;
 }
 
 bool check(int a[], int b[]) {
@@ -43,15 +38,15 @@ int main() {
     int ans=0;
     int dp[26]={};
     int foo[26]={};
-    unordered_map<int, bool>map;
+    unordered_map<ll, bool>map;
 
     for(char c : needle) {
         ++dp[c-97];
     }
     int l1=needle.length();
     int l2=haystack.length();
-    h=new int[l2+1];
-    p=new int[l2+1];
+    h=new ll[l2];
+    p=new ll[l2];
 
     if(l1>l2){
         cout << 0;
@@ -61,18 +56,18 @@ int main() {
     for(int i=0; i<l1; ++i) {
         foo[haystack[i]-97]++;
     }
-    hsh(haystack);
+    init(haystack);
+    ll hash;
     if(check(dp, foo)){
         ans++;
-        string temp=haystack.substr(0, l1);
-        map[id(0, l1-1)]=true;
+        hash=hsh(0, l1-1);
+        map[hash]=true;
     }
-    for(int i=0; i<l2-l1; ++i) {
-        foo[haystack[i]-97]--;
-        foo[haystack[i+l1]-97]++;
+    for(int i=1; i<l2-l1+1; ++i) {
+        foo[haystack[i-1]-97]--;
+        foo[haystack[i+l1-1]-97]++;
         if(check(dp, foo)){
-            string str=haystack.substr(i+1, l1);
-            int hash=id(i+1, i+l1);
+            hash=hsh(i, i+l1-1);
             if(!map[hash]){
                 ans++;
                 map[hash]=true;
