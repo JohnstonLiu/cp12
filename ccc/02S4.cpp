c365a26becbf300169dea410ac2f0cd1513389bb
Johnston Liu <johnstonliu2004@gmail.com>
2021-01-20 20:08:55 -0800

    cleanup

--- ccc/02S4.cpp
+++ ccc/02S4.cpp
@@ -1,104 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define INF 1000000009
+#define PB push_back
+#define MP make_pair
 
 int main() {
-    int N, K;
-    cin >> K >> N;
-    int a[N+1];
-    vector<string> names;
-    for(int i=0; i<N; ++i) {
-        string temp;
-        cin >> temp;
-        names.push_back(temp);
-        cin >> a[i];
-    }
-    int R=N%K;
-    int k[20]={};
-    int r[100]={};
-
-    int m=0;
-    for(int i=0; i<=N; ++i) {
-        if(i!=0 && i%K==0) {
-            k[i-K]=m;
-            m=0;
-        }
-        m=max(m, a[i]);
-    }
-    m=0;
-    for(int i=R; i<=N; ++i) {
-        if(i-R!=0 && (i-R)%K==0) {
-            k[i-K]=m;
-            m=0;
-        }
-        m=max(m, a[i]);
-    }
-    m=0;
-    int counter=1;
-    for(int i=0; i<=N; ++i, ++counter) {
-        if(i!=0 && counter==R+1) {
-            r[i-R]=m;
-            counter=1;
-            m=0;
-            i+=(K-R);
-        }
-        m=max(m, a[i]);
-    }
-    int format=0;
-    int sum=0;
-    sum+=r[0];
-    for(int i=R; i<N; i+=K) {
-        sum+=k[i];
-    }
-    int current=sum;
-    for(int i=0; i+K<N-1; i+=K) {
-        current-=r[i];
-        current+=k[i];
-        current-=k[i+R];
-        current+=r[i+K];
-        if(current<sum) {
-            sum=current;
-            format=i+K;
-        }
-    }
-    int final=0;
-    for(int i=0; i<N-R; i+=K) {
-        final+=k[i];
-    }
-    final+=r[N-R];
-    if(sum>final) {
-        sum=final;
-        format=N-R;
+    int M, Q;
+    cin >> M >> Q;
+    int a[Q];
+    string names[Q];
+    for(int i=0; i<Q; ++i) {
+        int n; string str;
+        cin >> str >> n;
+        a[i]=n;
+        names[i]=str;
     }
 
-    string outlier="";
-    for(int i=format; i<format+R; ++i) {
-        outlier+=names[i]+" ";
-        names[i]="";
-    }
-    for(int i=0; i<names.size(); ++i) {
-        if(names[i]=="") {
-            names.erase(names.begin()+i);
-            i--;
-        }
-    }
-    outlier+="\n";
-    names.push_back("");
-    cout << "Total Time: " << sum << "\n";
-    string output="";
-
-    for(int i=0; i<names.size(); i++) {
-        if(i!=0 && i%K==0){
-            output+="\n";
-        }
-        if(i==format) {
-            output+=outlier;
-        }
-        if(!names[i].empty()) {
-            output+=names[i]+" ";
-        }
-    }
-    if(output[output.size()-1]=='\n') {
-        output=output.substr(0, output.size()-2);
-    }
-    cout << output;
 }
--- ccc/19S4.cpp
+++ ccc/19S4.cpp
@@ -1,90 +1,24 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
-
-void print(int N, int a[], int k[], int r[]) {
-    for(int i=0; i<N; ++i) {
-        cout << a[i];
-    }
-    cout << endl;
-
-    for(int i=0; i<N; ++i) {
-        if(k[i]==0) {
-            cout << " ";
-        }else {
-            cout << k[i];
-        }
-    }
-    cout << endl;
-
-    for(int i=0; i<N; ++i) {
-        if(r[i]==0) {
-            cout << " ";
-        }else {
-            cout << r[i];
-        }
-    }
-}
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define INF 1000000009
+#define PB push_back
+#define MP make_pair
 
 int main() {
     int N, K;
     cin >> N >> K;
-    int a[1000010]={};
+    int a[N];
+    int days=N/K;
+    if(N%K!=0)days++;
+
     for(int i=0; i<N; ++i) {
-        cin >> a[i];
-    }    
-    int R=N%K;
+        a[i]=N;
+    }
 
-    int k[1000010]={};
-    int r[1000010]={};
-
-    int m=0;
-    for(int i=0; i<=N; ++i) {
-        if(i!=0 && i%K==0) {
-            k[i-K]=m;
-            m=0;
-        }
-        m=max(m, a[i]);
-    }
-    m=0;
-    for(int i=R; i<=N; ++i) {
-        if(i-R!=0 && (i-R)%K==0) {
-            k[i-K]=m;
-            m=0;
-        }
-        m=max(m, a[i]);
-    }
-    m=0;
-    int counter=1;
-    for(int i=0; i<=N; ++i, ++counter) {
-        if(i!=0 && counter==R+1) {
-            r[i-R]=m;
-            counter=1;
-            m=0;
-            i+=(K-R);
-        }
-        m=max(m, a[i]);
-    }
-    //print(N, a, k, r);
-
-    ll sum=0;
-    sum+=r[0];
-    for(int i=R; i<N; i+=K) {
-        sum+=k[i];
-    }
-    for(int i=0; i<N-R; i+=K) {
-        ll prev=sum;
-        sum-=r[i];
-        sum+=k[i];
-        sum-=k[i+R];
-        sum+=r[i+K];
-        sum=min(sum, prev);
-    }
-    ll final=0;
-    for(int i=0; i<N-R; i+=K) {
-        final+=k[i];
-    }
-    final+=r[N-R];
-    sum=min(sum, final);
-    cout << sum;
+    //knowing the amount of groups that I must form
+    //test possible group size arrangements
+    //rearrange them accordingly likely using recursion.
 }
