24de2c541364bc5b9001b7116a39108821d8b678
Johnston Liu <johnstonliu2004@gmail.com>
2021-02-10 15:34:39 -0800

    greedy bridge

--- ccc/02S4.cpp
+++ ccc/02S4.cpp
@@ -10,14 +10,28 @@ typedef pair<int, int> pi;
 int main() {
     int M, Q;
     cin >> M >> Q;
-    int a[Q];
-    string names[Q];
-    for(int i=0; i<Q; ++i) {
+    int a[Q+1];
+    string names[Q+1];
+    for(int i=1; i<=Q; ++i) {
         int n; string str;
         cin >> str >> n;
         a[i]=n;
         names[i]=str;
     }
-    //greedy alg to solve this
+    //int groups=Q/M+(Q%M!=0);
+    int best[Q+1];
+    best[0]=0;
+    for(int i=1; i<=Q; ++i)best[i]=INF;
+    int m=-INF;
 
+    for(int i=1; i<=Q; ++i) {
+        for(int j=i; j<i+M; ++j) {
+            m=max(m, a[j]);
+            best[j]=min(best[j], best[i-1]+m);
+        }
+        m=-INF;
+    }
+    //for(int i=1; i<=Q; ++i)cout << best[i] <<  " ";
+    //cout << endl;
+    cout << "Total Time: " << best[Q] << "\n";
 }
