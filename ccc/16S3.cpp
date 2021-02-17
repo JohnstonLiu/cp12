0838acd1a505fbf7b1b004361fd5bfee8b14e753
Johnston Liu <johnstonliu2004@gmail.com>
2021-02-16 20:31:00 -0800

    solved 16S3

--- ccc/16S3.cpp
+++ ccc/16S3.cpp
@@ -3,46 +3,56 @@ using namespace std;
 typedef long long ll;
 #define INF 1000000007
 
-struct node {
-    vector<int> next;
-    bool pho=false;
-};
-
-node graph[100001];
+vector<int> graph[100001];
 
 bool valid[100001]={};
-bool visited[100001]={};
-void prune(int s) {
-    node x=graph[s];
-    valid[s]=x.pho;
-    for(auto u : x.next) {
-        if(visited[u])continue;
-        visited[u]=true;
-        node a=graph[u];
-        valid[u]=a.pho;
-        if(valid[u])valid[s]=true;
-        prune(u);
+void prune(int s, int parent) {
+    for(auto u : graph[s]) {
+        if(u==parent)continue;
+        prune(u, s);
+        valid[s]|=valid[u];
     }
 }
-visited={};
+
+bool visited[100001]={};
+int start;
+int deep=0;
+int nodes;
+void dfs(int s, int depth) {
+    nodes++;
+    if(depth>deep) {
+        deep=depth;
+        start=s;
+    }
+    for(auto u : graph[s]) {
+        if(visited[u] || !valid[u])continue;
+        visited[u]=true;
+        dfs(u, depth+1);
+    }
+}
 
 int main() {
     int N, M;
     cin >> N >> M;
-    for(int i=1; i<=N; ++i) {
-        graph[i]=node();
-    }
+
     int p;
     for(int i=0; i<M; ++i) {
         int x; cin >> x;
-        graph[x].pho=true;
+        valid[x]=true;
         p=x;
     }
     for(int i=0; i<N-1; ++i) {
         int a, b;
         cin >> a >> b;
-        graph[a].next.push_back(b);
-        graph[b].next.push_back(a);
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
-    prune(p);
+    prune(p, -1);
+    dfs(p, 0);
+    memset(visited, 0, sizeof(visited));
+    deep=0;
+    nodes=0;
+    visited[start]=true;
+    dfs(start, 0);
+    cout << ((nodes-1)*2)-deep;
 }
