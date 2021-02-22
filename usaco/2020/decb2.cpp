03d2499f957147548dd51ae700f3d7c8f3ad9f4a
Johnston Liu <johnstonliu2004@gmail.com>
2021-02-21 21:34:19 -0800

    finished 2020 Dec B2

--- usaco/2020/decb2.cpp
+++ usaco/2020/decb2.cpp
@@ -5,5 +5,29 @@ typedef long long ll;
 
 int main() {
     int N; cin >> N;
-    
+    int sum[N+1];
+    sum[0]=0;
+    int a[N+1];
+    for(int i=1; i<=N; ++i) {
+        int x; cin >> x;
+        sum[i]=sum[i-1]+x;
+        a[i]=x;
+    }
+    //for(int i=1; i<=N; ++i)cout << sum[i] << " ";
+    int ans=N;
+    for(int i=1; i<N; ++i) {
+        for(int j=i+1; j<=N; ++j) {
+            int n=j-i+1;
+            int s=(sum[j]-sum[i-1]);
+            bool flag=false;
+            for(int k=i; k<=j; ++k) {
+                if(a[k]*n==s) {
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag)ans++;
+        }
+    }
+    cout << ans;
 }
