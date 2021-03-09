b5a51d699fc44f46effa33a48b4dc95bb5281cb8
Johnston Liu <johnstonliu2004@gmail.com>
2021-03-09 12:35:02 -0800

    setup cf

---
+++ codeforces/1216c.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INF 1000000007
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.sync_with_stdio(0);
+
+}
---
+++ usaco/2020/janb3.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INF 1000000007
+
+int main() {
+    //freopen("race.in", "r", stdin);
+    //freopen("race.out", "w", stdout);
+    int K, N; cin >> K >> N;
+
+}
+
